fc8ca3394aef72eee0a3ae1fd0d2b96f12c7ff53
HwangEyiKwon <hwang21035@naver.com>
2024-05-31 08:56:53 +0900

    [red&green] exception 처리 1

--- Project15/SimilarityChecker.cpp
+++ Project15/SimilarityChecker.cpp
@@ -1,9 +1,14 @@
 #include <string>
 #include <vector>
+#include <stdexcept>
 
 using namespace std;
 
 class SimilarityChecker {
 public:
-
+    int checkCharCount(string str1, string str2) {
+        for (const char& ch : str1) {
+            if (ch < 'A' || ch > 'Z') throw invalid_argument("Input Only A~Z!!!");
+        }
+    }
 };
--- Sample-Test1/SimilarityChecker_test.cpp
+++ Sample-Test1/SimilarityChecker_test.cpp
@@ -1,11 +1,10 @@
 #include "pch.h"
 #include "../Project15/SimilarityChecker.cpp"
 
-#include <string>
-#include <vector>
-
+#include <iostream>
 using namespace std;
 
-TEST(TestCaseName, TestName) {
-    EXPECT_EQ(1, 1);
+TEST(SimilarityCheckerTest, InvalidInputTest) {
+    SimilarityChecker sc;
+    EXPECT_THROW(sc.checkCharCount("2SCV", "74HDN3"), invalid_argument);
 }
