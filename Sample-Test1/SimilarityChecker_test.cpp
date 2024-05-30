483e67a4b014a5021e8630647399bc4df84ed67e
HwangEyiKwon <hwang21035@naver.com>
2024-05-31 08:40:43 +0900

    Default Setting!

---
+++ Sample-Test1/SimilarityChecker_test.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+#include "../Project15/SimilarityChecker.cpp"
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+TEST(TestCaseName, TestName) {
+    EXPECT_EQ(1, 1);
+}
