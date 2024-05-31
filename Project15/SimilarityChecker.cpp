33b08868cb6f84b383164e85ee30d85aa400de15
HwangEyiKwon <hwang21035@naver.com>
2024-05-31 09:47:47 +0900

    exception check

--- Project15/SimilarityChecker.cpp
+++ Project15/SimilarityChecker.cpp
@@ -1,9 +1,26 @@
 #include <string>
 #include <vector>
+#include <stdexcept>
 
 using namespace std;
 
 class SimilarityChecker {
 public:
+    int getAlphabetCheckPoint(string str1, string str2) {
+        checkIllegalArgument(str1, str2);
 
+        return 0;
+    }
+private:
+    void checkIllegalArgument(std::string& str1, std::string& str2)
+    {
+        for (const char& ch : str1) {
+            if (ch < 'A' || ch > 'Z') throw invalid_argument("Input Only A~Z!!!");
+        }
+        for (const char& ch : str2) {
+            if (ch < 'A' || ch > 'Z') throw invalid_argument("Input Only A~Z!!!");
+        }
+
+        if (str1.size() > 10 || str2.size() > 10) throw length_error("Input Size must be under 10!!!");
+    }
 };
--- Sample-Test1/SimilarityChecker_test.cpp
+++ Sample-Test1/SimilarityChecker_test.cpp
@@ -1,11 +1,24 @@
 #include "pch.h"
 #include "../Project15/SimilarityChecker.cpp"
 
-#include <string>
-#include <vector>
+class SimilarityCheckerTestFixture : public testing::Test {
+public:
+    SimilarityChecker stSimilarirtyChecker;
+    void assertIllegalArgument(string str1, string str2) {
+        try {
+            stSimilarirtyChecker.getAlphabetCheckPoint(str1, str2);
+            FAIL();
+        }
+        catch (exception e) {
+            // PASS
+        }
+    }
+};
 
-using namespace std;
-
-TEST(TestCaseName, TestName) {
-    EXPECT_EQ(1, 1);
+TEST_F(SimilarityCheckerTestFixture, AssertIllegalArgumentTest) {
+    assertIllegalArgument("2SCV", "74HDN3");
+    assertIllegalArgument("SCV", "74HDN3");
+    assertIllegalArgument("2SCV", "HDN");
+    assertIllegalArgument("JASLFAJKSDLFJSCV", "HDN");
+    assertIllegalArgument("ASD", "JASLFAJKSDLFJSCV");
 }
