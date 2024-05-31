7ff89b0183ca343aaa43a485152ed8c2c75c9e71
HwangEyiKwon <hwang21035@naver.com>
2024-05-31 09:06:28 +0900

    refactoring 추가

--- Project15/SimilarityChecker.cpp
+++ Project15/SimilarityChecker.cpp
@@ -7,6 +7,10 @@ using namespace std;
 class SimilarityChecker {
 public:
     int checkCharCount(string str1, string str2) {
+        checkIllegalArgument(str1, str2);
+    }
+    void checkIllegalArgument(std::string& str1, std::string& str2)
+    {
         for (const char& ch : str1) {
             if (ch < 'A' || ch > 'Z') throw invalid_argument("Input Only A~Z!!!");
         }
